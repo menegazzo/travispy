f46596a0255d14dd76ec682fbc48db32817203b3
Fabio Menegazzo <menegazzo@gmail.com>
2014-07-01 16:09:50 -0300

    Adapting to have coverage.

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+
+    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(['travispy'] + self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -26,5 +47,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy',
-    ]
+    ],
+    
+    # tests
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
